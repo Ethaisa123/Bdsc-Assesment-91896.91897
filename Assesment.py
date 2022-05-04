5905a0581810969edb0becd39b38ca24c65fdb72
Ethan van Wijk <ethanvanwijknz@gmail.com>
2022-05-05 10:49:07 +1200

    test commit 1

--- Assesment.py
+++ Assesment.py
@@ -1,3 +1,3 @@
 
 #the list is formatted as [class, tech_value, arts_value, health, writing_value]
-classes = [["programming",10, 1, 0, 4 ],["pe", 1, 1, 10, 3],["textiles", 3, 9, 2, 3],[],[]]
+classes = [["programming",10, 1, 0, 4 ],["pe", 1, 1, 10, 3],["textiles", 3, 9, 2, 3],["class4", 1,1,1],[]]
