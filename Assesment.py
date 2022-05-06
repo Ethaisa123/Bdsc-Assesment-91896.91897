7559b3cf6239f8998a48c7cf41470c6c2e9da36d
Ethan van Wijk <ethanvanwijknz@gmail.com>
2022-05-06 13:44:38 +1200

    update22

--- Assesment.py
+++ Assesment.py
@@ -1,3 +1,9 @@
 
 #the list is formatted as [class, tech_value, arts_value, health, writing_value]
-classes = [["programming",10, 1, 0, 4 ],["pe", 1, 1, 10, 3],["textiles", 3, 9, 2, 3],["class4", 1,1,1],[]]
+classes = {"programming": [10, 1, 0, 4 ],"pe": [1, 1, 10, 3],"textiles": [3, 9, 2, 3],"hisotry": [2,1,2,9],"physics":[5,0,1,7]}
+careers = {"programming":[10, 1, 0, 4 ],"pe": [1, 1, 10, 3],"textiles": [3, 9, 2, 3],"hisotry": [2,1,2,9],"physics":[5,0,1,7]}
+
+
+chosen_career = input("What is you chosen career")
+
+print(careers[chosen_career])
