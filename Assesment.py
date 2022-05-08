fd8fa5bcb75d56805d9e7a287ee4d1437bacf4b7
Ethan van Wijk <ethanvanwijknz@gmail.com>
2022-05-09 09:15:06 +1200

    update 3

--- Assesment.py
+++ Assesment.py
@@ -4,6 +4,9 @@ classes = {"programming": [10, 1, 0, 4 ],"pe": [1, 1, 10, 3],"textiles": [3, 9, 
 careers = {"programming":[10, 1, 0, 4 ],"pe": [1, 1, 10, 3],"textiles": [3, 9, 2, 3],"hisotry": [2,1,2,9],"physics":[5,0,1,7]}
 
 
-chosen_career = input("What is you chosen career")
+#chosen_career = input("What is you chosen career")
 
-print(careers[chosen_career])
+#print(careers[chosen_career])
+
+print(classes["programming"][0])
+print(careers["programming"][0])
