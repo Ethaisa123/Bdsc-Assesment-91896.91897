ee7d566554568ccffac81f68f4142c83d37c7190
Ethan van Wijk <ethanvanwijknz@gmail.com>
2022-06-01 09:35:36 +1200

    fixed varios things

--- Assesment.py
+++ Assesment.py
@@ -11,42 +11,47 @@ def getList(dict):
 # Driver program
 classes_list_temp = getList(classes)
 print(type(classes_list_temp))
+
+
+career_list_temp = getList(careers)
+print(type(career_list_temp))
 #chosen_career = input("What is you chosen career")
 diff_var = 100000
 best_class = ""
 #print(careers[chosen_career])
 
-print(classes["programming"][0])
-print(careers["programming"][0])
-differences = []
+
 difference_total = 0
 
 
-print("for i in: \n")
+#VARIABLES DEFINED
 my_class = "hisotry"
-career = "textiles"
+career = career_list_temp[0]
+#
 class_num = len(classes_list_temp)
 print(class_num)
 print(classes_list_temp)
 
 for i in range(0,class_num):
     print(i)
-    classes_list_temp[i]
+    print(career_list_temp[i])
+    career = career_list_temp[i]
     my_class = classes_list_temp[i]
     for i in range(0,4):
         
         #print("class attribute: {}" .format(classes[my_class][i]))
         #print("career attribute: {}".format(careers[career][i]))
-        differences.append(abs(classes[my_class][i] - careers[career][i]))
         difference_total = difference_total + abs(classes[my_class][i] - careers[career][i])
 
     #print("differences: " + str(differences))
-    print("total of current class: " + str(difference_total))
+    print("currentclass: {}" .format(best_class))
+    print("current difference: " + str(difference_total))
     if difference_total < diff_var:
         best_class = my_class
+        
         diff_var = difference_total
-        print("This is the current best class:{} \n" .format(my_class))
-    print("this is the best classes value: {}" .format(diff_var))
+        print("lowwest difference:{} \n" .format(my_class))
+    print("lowwest difference: {}" .format(diff_var))
 
 print("THE BEST CLASS IS: {}" .format(best_class))
 
