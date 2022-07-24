f93090eb6400bbcaffd2215fc04ee6d51defceff
Ethan van Wijk <100451791+Ethaisa123@users.noreply.github.com>
2022-07-24 15:50:53 +1200

    this is a commit

--- main + data/Assesment.py
+++ main + data/Assesment.py
@@ -2,34 +2,50 @@
 from pandas import *
  
 # reading CSV file
-data = read_csv("data-careers.csv")
+data = read_csv("main + data\data-careers.csv")
  
-# converting column data to list
+#this code takes the column names from the csv file and adds them to their own list in order
+#this first line adds all of the names of the carrer choices and adds them to a list in the format (career1,career2,career3,ect...)
 name = data['class name'].tolist()
+#the next 4 lines of code do roughly the same thing but they get the "heat values" (ill explain later) instead of names in the format (1.0,3.0,2.0)
 data_1 = data['commerce'].tolist()
 data_2 = data['writing'].tolist()
 data_3 = data['stem'].tolist()
 data_4 = data['arts'].tolist()
- 
-# printing list data
+#these lists are made in such a way that the first variable of each list match and and so on from each concecutive pair
+
+
+# this is testing code that is used to make sure the values are lined up correctly
+"""
 name_list = ('class name', name)
-print('Facewash', data_1)
-print('Toothpaste', data_2)
-print('Shampoo', data_3)
+print('commerce', data_1)
+print('writing', data_2)
+print('stem', data_3)
 print('arts', data_4)
+print(name_list)
+"""
+#this is the final dictionary that the excel file will be writen to
+classes_list_data = {}
+
+#this code takes the data from name and data lists and turns them into a format that is easily usable in future code the format is {"name of class: [int1,int2,int3,int4]"} (the ints corrispond to heat values)
+for i in range (0, len(name)):
+    classes_list_data[name[i]] = [data_1[i], data_2[i], data_3[i], data_4[i]] 
+print(classes_list_data)
 
 
-#the list is formatted as [class, tech_value, arts_value, health, writing_value]
+# this is a test list i did to figure out the formatting
+"""
 classes_list = {"programming": [10, 1, 0, 4 ],"pe": [1, 1, 10, 3],"textiles": [3, 9, 2, 3],"hisotry": [2,1,2,9],"physics":[5,0,1,7]}
+"""
 careers = {"programming":[10, 1, 0, 4 ],"pe": [1, 1, 10, 3],"textiles": [3, 9, 2, 3],"hisotry": [2,1,2,9],"physics":[5,0,1,7]}
 
-#creating a temporary list from the keys of the dicctionary
+#this code creats a temporary list from the keys of the dicctionary
 
 def getList(dict):
     return list(dict.keys())
 
 # Driver program
-classes_list_temp = getList(classes_list)
+classes_list_temp = getList(classes_list_data)
 print(type(classes_list_temp))
 
 #what
